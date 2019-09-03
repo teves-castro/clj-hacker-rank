d328cec96a2787aa640f39d8749836891ef4f244
Vítor Castro <vcastro70@gmail.com>
2019-09-03 23:07:56 +0100

    add failing case

--- src/hacker_rank/arraymanipulation.clj
+++ src/hacker_rank/arraymanipulation.clj
@@ -3,29 +3,32 @@
 
 (set! *warn-on-reflection* true)
 
-(defn apply_operation [[lower upper value] array]
+(defn apply_operation [array [lower upper value]]
   (let [updated (update array lower + value)]
     (if (< upper (count array))
       (update updated (inc upper) - value)
       updated)))
 
-(defn process-operations [initial-ops n]
+(defn process-operations [n initial-ops]
   (reduce
-   (fn [acc cur] (apply_operation cur acc))
-   (vec (int-array (inc n)))
+   apply_operation
+   (->> n inc range vec)
    initial-ops))
 
 (defn calculate-max [array]
-  (second (reduce
-           (fn [[acc max] cur]
-             (let [tmp (+ acc cur)]
-               [tmp (if (> tmp max) tmp max)]))
-           [0 0]
-           array)))
+  (->> array
+       (reduce
+        (fn [[acc m] cur]
+          (let [tmp (+ acc cur)]
+            [tmp (max tmp m)]))
+        [0 0])
+       second))
 
 ; Complete the arrayManipulation function below.
-(defn arrayManipulation' [[[_ n] & queries]]
-  (calculate-max (process-operations queries (inc n))))
+(defn arrayManipulation [[[_ n] & queries]]
+  (->> queries
+       (process-operations (inc n))
+       calculate-max))
 
 (defn apply-op [vals [from to op]]
   ; (let [ini (subvec vals 0 from)
@@ -34,15 +37,13 @@
   ;   (vec (concat ini mid end))))
   (map-indexed (fn [i v] (if (and (>= (inc i) from) (<= (inc i) to)) (+ v op) v)) vals))
 
-(apply-op [0 0 0 0 0] [1 3 1])
-
-(defn arrayManipulation [[[_ n] & queries]]
+(defn arrayManipulation' [[[_ n] & queries]]
   (->> queries
        (reduce apply-op (vec (repeat (inc n) 0)))
        (apply max)))
 
 (defn read-lines []
-  (with-open [rdr (clojure.java.io/reader "src/hacker_rank/operations.txt")]
+  (with-open [rdr (clojure.java.io/reader "src/hacker_rank/operations-large.txt")]
     (reduce conj [] (line-seq rdr))))
 
 (defn parse-line [l] (map read-string (str/split l #" ")))
@@ -50,8 +51,13 @@
 (defn read-ops []
   (->> (read-lines)
        (map parse-line)
+      ;  (take 2)
        (map vec)))
 
 (->> (read-ops)
-     (arrayManipulation')
+     (arrayManipulation)
      (time))
+
+; (arrayManipulation [[10000000 100000] [7253005 9867484 26205]])
+
+; (->> 10000000 range)
