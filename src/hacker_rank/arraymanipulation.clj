b3cf5aeffca2bff0021c8e212eaf66d01aae7d11
Vitor Castro <vcastro@equalexperts.com>
2019-09-04 13:38:29 +0100

    solve array manipulation

--- src/hacker_rank/arraymanipulation.clj
+++ src/hacker_rank/arraymanipulation.clj
@@ -1,46 +1,25 @@
 (ns hacker-rank.arraymanipulation
   (:require [clojure.string :as str]))
 
-(set! *warn-on-reflection* true)
+(defn apply_operation [m [lower upper value]]
+  (let [afterUpper (inc upper)]
+    (assoc m
+           lower (+ (m lower 0) value)
+           afterUpper (- (m afterUpper 0) value))))
 
-(defn apply_operation [array [lower upper value]]
-  (let [updated (update array lower + value)]
-    (if (< upper (count array))
-      (update updated (inc upper) - value)
-      updated)))
-
-(defn process-operations [n initial-ops]
-  (reduce
-   apply_operation
-   (->> n inc range vec)
-   initial-ops))
-
-(defn calculate-max [array]
-  (->> array
-       (reduce
-        (fn [[acc m] cur]
+(defn calculate-max [ops]
+  (->> ops
+       (reduce-kv
+        (fn [[acc m] _ cur]
           (let [tmp (+ acc cur)]
             [tmp (max tmp m)]))
         [0 0])
        second))
 
-; Complete the arrayManipulation function below.
-(defn arrayManipulation [[[_ n] & queries]]
-  (->> queries
-       (process-operations (inc n))
+(defn arrayManipulation [[[n _] & ops]]
+  (->> ops
+       (reduce apply_operation (sorted-map))
        calculate-max))
-
-(defn apply-op [vals [from to op]]
-  ; (let [ini (subvec vals 0 from)
-  ;       mid (map (fn [x] (+ x op)) (subvec vals from (inc to)))
-  ;       end (subvec vals (inc to))]
-  ;   (vec (concat ini mid end))))
-  (map-indexed (fn [i v] (if (and (>= (inc i) from) (<= (inc i) to)) (+ v op) v)) vals))
-
-(defn arrayManipulation' [[[_ n] & queries]]
-  (->> queries
-       (reduce apply-op (vec (repeat (inc n) 0)))
-       (apply max)))
 
 (defn read-lines []
   (with-open [rdr (clojure.java.io/reader "src/hacker_rank/operations-large.txt")]
@@ -51,13 +30,12 @@
 (defn read-ops []
   (->> (read-lines)
        (map parse-line)
-      ;  (take 2)
        (map vec)))
 
 (->> (read-ops)
      (arrayManipulation)
      (time))
 
-; (arrayManipulation [[10000000 100000] [7253005 9867484 26205]])
+(time (arrayManipulation [[10000000 1] [7253005 9867484 26205]]))
 
 ; (->> 10000000 range)
